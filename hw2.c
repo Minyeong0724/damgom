7d6a072b4e8c6480d37e57fda7cfbd62df1814e8
Minyeong0724 <149355272+Minyeong0724@users.noreply.github.com>
2024-06-16 22:55:07 +0900

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -4,13 +4,10 @@
 // YOU WILL TURN IN THIS FILE.
 // Read the provided instruction carefully.
 
-
-// Applying inline function
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <omp.h>
+#include <omp.h>
 
 #include "bmplib.h"
 #include "hw2.h"
@@ -18,41 +15,58 @@
 // This implementation is simply copied from "main.c".
 // Your job is to modify and optimize it for better performance!
 
-static inline void apply_filter(Pixel* input, int nx, int ny, int width, int height, float* filter, int* r, int* g, int* b, int filter_index) {
-    // 경계를 넘는 경우를 처리
-    if (nx < 0 || nx >= width || ny < 0 || ny >= height) return;
-
-    int pixel_index = nx + ny * width;
-    *r += input[pixel_index].r * filter[filter_index];
-    *g += input[pixel_index].g * filter[filter_index];
-    *b += input[pixel_index].b * filter[filter_index];
-}
-
 static Pixel convolution(
         Pixel* input, int x, int y, int width, int height, float* filter) {
     // double r = 0;
     // double g = 0;
     // double b = 0;
-    int r = 0; 
+    int r = 0;
     int g = 0;
-    int b = 0; 
+    int b = 0;
+
+
+    // TODO
+    // [1] 반복 연산 local variable로 바꿔서 하기
+    // [2] loop~goto label? 
+    // [3] inline function
+    // [4] int<< 계산 빠름(1 pixel까지 오차범위 허용) | *, / -> +, -
+    // [5] for문의 끝을 알고 있으면 for 쓰지말고 걍 더하기 존내하기
+    // -> 범위 계산 필요 x
+    // [6] Seperate allocators << ㅇ-ㅇ.. 
 
     // trial1 || 128: 1.23 ~ 1.45 || others: 1.24
     // :memeory allocation optimization, for loop opt(y first), pre-calculate filter_index, pixel_index
 
     // trial2 || 128: 1.23 ~ 1.48 || 256: 1.23 ~ 1.4 || others: 1.23
     // :allocate value of r,g,b to output directly. (No use of pixel* p.)
-    // +) using inline function & eliminate for loop (but same...... )
 
-    apply_filter(input, x-1, y-1, width, height, filter, &r, &g, &b, 0);
-    apply_filter(input, x,   y-1, width, height, filter, &r, &g, &b, 1);
-    apply_filter(input, x+1, y-1, width, height, filter, &r, &g, &b, 2);
-    apply_filter(input, x-1, y,   width, height, filter, &r, &g, &b, 3);
-    apply_filter(input, x,   y,   width, height, filter, &r, &g, &b, 4);
-    apply_filter(input, x+1, y,   width, height, filter, &r, &g, &b, 5);
-    apply_filter(input, x-1, y+1, width, height, filter, &r, &g, &b, 6);
-    apply_filter(input, x,   y+1, width, height, filter, &r, &g, &b, 7);
-    apply_filter(input, x+1, y+1, width, height, filter, &r, &g, &b, 8);
+    for (int dx = -1; dx <= 1; ++dx) {
+        int nx = x + dx;
+        for (int dy = -1; dy <= 1; ++dy) {
+            if(nx < 0 || nx >= width) continue;
+            int ny = y + dy;
+            if(ny < 0 || ny >= height) continue;
+
+            // if(x + dx < 0 || x + dx >= width || y + dy < 0 || y + dy >= height) continue;
+            // if(y + dy < 0 || y + dy >= height) continue;
+
+            // if(nx < 0 || nx >= width || ny < 0 || ny >= height) continue;
+
+            int filter_index = (dx + 1) + (dy + 1) * 3;
+            // int pixel_index = x + dx + (y + dy) * width;
+            int pixel_index = nx + ny * width;
+            // double helper = filter[filter_index];
+            // r += input[pixel_index].r * helper;
+            // g += input[pixel_index].g * helper;
+            // b += input[pixel_index].b * helper;
+            r += input[pixel_index].r * filter[filter_index];
+            g += input[pixel_index].g * filter[filter_index];
+            b += input[pixel_index].b * filter[filter_index];
+            // r += input[(x+dx)+(y+dy)*width].r * filter[(dx+1)+(dy+1)*3];
+            // g += input[(x+dx)+(y+dy)*width].g * filter[(dx+1)+(dy+1)*3];
+            // b += input[(x+dx)+(y+dy)*width].b * filter[(dx+1)+(dy+1)*3];
+        }
+    }
 
     if (r < 0) r = 0;
     if (g < 0) g = 0;
@@ -85,7 +99,7 @@ void filter_optimized(void* args[]) {
     Pixel* output = args[3];
     float* filter = args[4];
 
-    // #pragma omp parallel for collapse(2)
+    #pragma omp parallel for collapse(2)
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             output[x + y * width] = convolution(input, x, y, width, height, filter);
